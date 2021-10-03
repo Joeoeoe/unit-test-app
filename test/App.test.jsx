3f3361ca00ce045e47d79b3ec13028b1cec9a95f
Joeoeoe <zhou_lihan@163.com>
2021-10-03 11:49:27 +0800

    添加自定义ReactDOM.render例子，可以正常显示；并且发现RTL渲染会带有很多暂不知道什么意思的代码，后续研究

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../src/App.jsx';
 
@@ -12,13 +13,18 @@ import App from '../src/App.jsx';
 //   
 /**
  * // TODO
- * 1.为什么用React浏览器没有显示出内容？而原生DOM就有
+ * 1.为什么用React浏览器没有显示出内容？而原生DOM就有——> 使用ReactDom.render可以成功渲染，但是使用RTL就没办法？并且会带有很多其他代码，估计是不兼容Karma？
  * 2.jest、jest-dom、jsdom关系
  */
 describe('react test', function () {
-  it('render react', function () {
-    render(<App />);
+  // it('render react with RTL', function () {
+  //   render(<App />);
+  //   screen.debug();
+  //   screen.getByText('hello world');
+  // })
+
+  it('render react with custom render', function () {
+    ReactDOM.render(<App />, document.body);
     screen.debug();
-    screen.getByText('hello world');
   })
 })
