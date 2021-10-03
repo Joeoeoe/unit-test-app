9791bdf116076de1b5f2c46a6e24ef98636bed82
Joeoeoe <zhou_lihan@163.com>
2021-10-03 13:35:35 +0800

    更新猜测

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -13,20 +13,20 @@ import App from '../src/App.jsx';
 //   
 /**
  * // TODO
- * 1.为什么用React浏览器没有显示出内容？而原生DOM就有——> 使用ReactDom.render可以成功渲染，但是使用RTL就没办法？并且会带有很多其他代码，代码把所有的script标签都打印了出来
+ * 1.为什么用React浏览器没有显示出内容？而原生DOM就有——> 使用ReactDom.render可以成功渲染，但是使用RTL就没办法？并且会带有很多其他代码，代码把所有的script标签都打印了出来；猜测：是不是RTL源码中act的原因？
  * 2.jest、jest-dom、jsdom关系
  */
 describe('react test', function () {
-  // it('render react with RTL', function () {
-  //   render(<App />);
-  //   screen.debug();
-  //   screen.getByText('hello world');
-  // })
-
-  it('render react with custom render', function () {
-    const div = document.createElement('div');
-    document.body.append(div);
-    ReactDOM.render(<App />, div);
+  it('render react with RTL', function () {
+    render(<App />);
     screen.debug();
+    screen.getByText('hello world');
   })
+
+  // it('render react with custom render', function () {
+  //   const div = document.createElement('div');
+  //   document.body.append(div);
+  //   ReactDOM.render(<App />, div);
+  //   screen.debug(div);
+  // })
 })
