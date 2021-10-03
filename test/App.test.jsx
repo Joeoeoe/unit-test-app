88ecb362314a7d7cb41c21b33d9520f8387742b1
Joeoeoe <zhou_lihan@163.com>
2021-10-03 22:38:33 +0800

    update todo RTL render疑惑更新

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -19,10 +19,37 @@ import { act } from 'react-dom/test-utils';
  * 3.是否要把css文件等也解析打包进去？有什么意义？优点：可以看见自动化测试过程；缺点：测试速度会变慢，会变慢多少？
  */
 describe('react test', function () {
+  // TODO 从RTL中抽取出来的代码，为啥能渲染到浏览器中，而使用render方法则不行？？
   it('render react with RTL', function () {
-    render(<App />);
+
+    let baseElement = null;
+    let container = null;
+    const hydrate = false;
+    const ui = <App />;
+
+
+    if (!baseElement) {
+      // default to document.body instead of documentElement to avoid output of potentially-large
+      // head elements (such as JSS style blocks) in debug output
+      baseElement = document.body
+    }
+    if (!container) {
+      container = baseElement.appendChild(document.createElement('div'))
+    }
+
+  
+    act(() => {
+      if (hydrate) {
+        ReactDOM.hydrate(ui, container)
+      } else {
+        ReactDOM.render(ui, container)
+      }
+    })
+
+    const div = document.body.appendChild(document.createElement('div'));
+    render(<p>123456</p>, { container: div });
     screen.debug();
-    screen.getByText('hello world');
+    // screen.getByText('hello world');
   })
 
   // it('render react with custom render', function () {
