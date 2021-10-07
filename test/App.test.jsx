fac312927917dd34be2a2de8b318d13a82ba9d2a
Joeoeoe <zhou_lihan@163.com>
2021-10-07 17:32:55 +0800

    解决chai exist疑惑

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -84,7 +84,22 @@ describe('react test', function () {
 
     // toBeInTheDocument是jest-dom的API，与jest集成，故在chai中无法使用
     // expect(btnElement).toBeInTheDocument();
-    // TODO 为什么chai-dom这里是这样
+    /**
+     * 为什么chai-dom这里是这样? 
+     * 原因：expect返回Assertion对象，to是为了方便可读性所做的chain，其值仍然是Assertion对象
+     * 最后读取exist值，chai-dom在这里用overwriteProperty做了一层封装判断DOM是否存在。
+     * overwriteProperty底层用了Object.defineProperty设置属性的getter，getter调用了chai-dom
+     * 重写的方法，故能进行判断
+     * 
+     * 另外，chai原本定义exist的时候就是使用了addProperty，为其设置了getter，而不是使用addMethod把它作为方法，
+     * 所以chai-dom就依照他的规则是用了overwriteProperty。
+     * 
+     * 至于为什么chai使用exist，目前尚不清楚
+     * 
+     * chai文档对于exist的说明：
+     * Asserts that the target is not strictly (===) equal to either null or undefined.
+     * However, it’s often best to assert that the target is equal to its expected value.
+     */
     expect(btnElement).to.exist;
   })
 })
