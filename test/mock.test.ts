be69748f74192dbba22a9d4bccadd72819647d8f
Joeoeoe <zhou_lihan@163.com>
2021-10-09 23:48:40 +0800

    sinon-chai配置完成，但是每个单测前均需要chai.use，看看是否有其他方法

--- test/mock.test.ts
+++ test/mock.test.ts
@@ -1,3 +1,7 @@
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+chai.use(sinonChai);
+
 function once(fn) {
     var returnValue,
         called = false;
@@ -17,4 +21,7 @@ it("calls the original function", function () {
 
     // 原生sinon写法
     expect(callback.called).to.be.ok;
+
+    // sinon-chai写法
+    expect(callback).to.have.been.called;
 });
