11f169fe728606c86e8d081647e8d807963c7877
Joeoeoe <zhou_lihan@163.com>
2021-12-25 21:52:57 +0800

    feat: 添加jest mock getBoundingClientRect

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -9,13 +9,19 @@ import App from './App';
 //   expect(btnElement).toBeInTheDocument();
 // });
 
-test('browser API in jsdom', () => {
+// test('browser API in jsdom', () => {
+//   render(<App />);
+//   const btnElement = screen.getByText(/click me/i);
+//   userEvent.click(btnElement);
+// });
+
+test('mock browser API in jsdom', () => {
   render(<App />);
+
+  // mock getBoundingClientRect函数
+  const getBoundingClientRectSpy = jest.fn(() => ({ top: 80 } as DOMRect));
+  window.HTMLElement.prototype.getBoundingClientRect = getBoundingClientRectSpy;
+
   const btnElement = screen.getByText(/click me/i);
-  /**
-   * 点击后在jsdom环境中返回：{ bottom: 0, height: 0, left: 0, right: 0, top: 0, width: 0 }
-   * 在真实浏览器环境中应该是：{"x":8,"y":88,"width":64.140625,"height":21,"top":88,"right":72.140625,"bottom":109,"left":8}
-   * 如此导致某些库（如popper.js）在使用数据计算时会返回NAN（如0/0）
-   */
   userEvent.click(btnElement);
 });
