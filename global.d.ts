ad903e1b83c3293509b65cc637f18cda832c9c5f
Joeoeoe <zhou_lihan@163.com>
2022-01-10 20:22:49 +0800

    feat: 更新chai类型引入

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,13 @@
+import Chai from 'chai';
+declare global {
+    const expect: Chai.ExpectStatic;
+    declare module '*.css' {
+        const content: { [className: string]: string };
+        export default content;
+    }
 
-declare module '*.css' {
-    const content: { [className: string]: string };
-    export default content;
-}
-
-declare module '*.less' {
-    const content: { [className: string]: string };
-    export default content;
+    declare module '*.less' {
+        const content: { [className: string]: string };
+        export default content;
+    }
 }
